8158168e84d1d0a1a9bbc3123e072e597b4d2503
joanneBlocks <jscosto@gmail.com>
2026-04-05 03:32:25 +0800

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/categoriesController');
 
-router.get('/', ctrl.listCategories);
-router.get('/:id', ctrl.getCategory);
+router.get('/',            ctrl.listCategories);
+router.get('/new',         ctrl.newCategoryForm);
+router.get('/:id',         ctrl.getCategory);
+router.get('/:id/edit',    ctrl.editCategoryForm);
+router.post('/',           ctrl.createCategory);
+router.post('/:id/update', ctrl.updateCategory);
+router.post('/:id/delete', ctrl.deleteCategory);
 
 module.exports = router;
