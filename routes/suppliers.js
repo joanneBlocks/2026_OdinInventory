87f7514d8e35d7b855ea70b5890bed3afd9c5412
joanneBlocks <jscosto@gmail.com>
2026-04-05 03:17:03 +0800

    Update suppliers.js

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const ctrl = require('../controllers/suppliersController');
+
+router.get('/', ctrl.listSuppliers);
+router.get('/:id', ctrl.getSupplier);
 
 module.exports = router;
