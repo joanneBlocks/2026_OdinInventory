d5df0d733892dda60174781ef122225116af5167
joanneBlocks <jscosto@gmail.com>
2026-04-05 03:18:43 +0800

    Update items.js

--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const ctrl = require('../controllers/itemsController');
+
+router.get('/', ctrl.listItems);
+router.get('/:id', ctrl.getItem);
 
 module.exports = router;
