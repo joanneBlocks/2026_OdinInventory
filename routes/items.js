4a55ee6b5533d3320cece9666c24283899389dd0
joanneBlocks <jscosto@gmail.com>
2026-04-05 03:32:28 +0800

    Update items.js

--- routes/items.js
+++ routes/items.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/itemsController');
 
-router.get('/', ctrl.listItems);
-router.get('/:id', ctrl.getItem);
+router.get('/',            ctrl.listItems);
+router.get('/new',         ctrl.newItemForm);
+router.get('/:id',         ctrl.getItem);
+router.get('/:id/edit',    ctrl.editItemForm);
+router.post('/',           ctrl.createItem);
+router.post('/:id/update', ctrl.updateItem);
+router.post('/:id/delete', ctrl.deleteItem);
 
 module.exports = router;
