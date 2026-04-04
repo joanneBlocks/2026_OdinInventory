6499bf764ba07ef83b287a5919a1164abd2683fc
joanneBlocks <jscosto@gmail.com>
2026-04-05 04:10:34 +0800

    Update suppliers.js

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/suppliersController');
+const requireAdmin = require('../middleware/adminAuth');
 
 router.get('/',            ctrl.listSuppliers);
 router.get('/new',         ctrl.newSupplierForm);
 router.get('/:id',         ctrl.getSupplier);
 router.get('/:id/edit',    ctrl.editSupplierForm);
-router.post('/',           ctrl.createSupplier);
-router.post('/:id/update', ctrl.updateSupplier);
-router.post('/:id/delete', ctrl.deleteSupplier);
+router.post('/',           requireAdmin, ctrl.createSupplier);
+router.post('/:id/update', requireAdmin, ctrl.updateSupplier);
+router.post('/:id/delete', requireAdmin, ctrl.deleteSupplier);
 
 module.exports = router;
