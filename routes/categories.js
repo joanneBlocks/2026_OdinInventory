77f3b7193d76cf70392897b5452d4841010bac66
joanneBlocks <jscosto@gmail.com>
2026-04-05 03:15:44 +0800

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const ctrl = require('../controllers/categoriesController');
+
+router.get('/', ctrl.listCategories);
+router.get('/:id', ctrl.getCategory);
 
 module.exports = router;
