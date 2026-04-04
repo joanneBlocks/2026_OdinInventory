8e162b2b647a38535beb601da4ae9db9b47394f8
joanneBlocks <jscosto@gmail.com>
2026-04-05 05:34:33 +0800

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
+  ssl: process.env.NODE_ENV === 'production' || process.env.DATABASE_URL.includes('render.com')
     ? { rejectUnauthorized: false }
     : false,
 });
