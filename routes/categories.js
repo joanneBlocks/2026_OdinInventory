91b63f9cb5be8d9238b333bda892f96860c904ec
joanneBlocks <jscosto@gmail.com>
2026-04-05 04:10:30 +0800

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/categoriesController');
+const requireAdmin = require('../middleware/adminAuth');
 
 router.get('/',            ctrl.listCategories);
 router.get('/new',         ctrl.newCategoryForm);
 router.get('/:id',         ctrl.getCategory);
 router.get('/:id/edit',    ctrl.editCategoryForm);
-router.post('/',           ctrl.createCategory);
-router.post('/:id/update', ctrl.updateCategory);
-router.post('/:id/delete', ctrl.deleteCategory);
+router.post('/',           requireAdmin, ctrl.createCategory);
+router.post('/:id/update', requireAdmin, ctrl.updateCategory);
+router.post('/:id/delete', requireAdmin, ctrl.deleteCategory);
 
 module.exports = router;
