805820c5a358a11bb379f35a14a449953786517f
joanneBlocks <jscosto@gmail.com>
2026-04-05 03:32:30 +0800

    Update suppliers.js

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/suppliersController');
 
-router.get('/', ctrl.listSuppliers);
-router.get('/:id', ctrl.getSupplier);
+router.get('/',            ctrl.listSuppliers);
+router.get('/new',         ctrl.newSupplierForm);
+router.get('/:id',         ctrl.getSupplier);
+router.get('/:id/edit',    ctrl.editSupplierForm);
+router.post('/',           ctrl.createSupplier);
+router.post('/:id/update', ctrl.updateSupplier);
+router.post('/:id/delete', ctrl.deleteSupplier);
 
 module.exports = router;
