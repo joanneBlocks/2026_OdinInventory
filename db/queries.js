c93ae4b9e5ace127d7dccbe581c6336182b78d51
joanneBlocks <jscosto@gmail.com>
2026-04-05 03:15:42 +0800

    Update queries.js

--- db/queries.js
+++ db/queries.js
@@ -14,3 +14,18 @@ exports.getCategoryById = async (id) => {
   );
   return rows[0];
 };
+
+// Suppliers
+exports.getAllSuppliers = async () => {
+  const { rows } = await pool.query(
+    'SELECT * FROM suppliers ORDER BY name'
+  );
+  return rows;
+};
+
+exports.getSupplierById = async (id) => {
+  const { rows } = await pool.query(
+    'SELECT * FROM suppliers WHERE id = $1', [id]
+  );
+  return rows[0];
+};
